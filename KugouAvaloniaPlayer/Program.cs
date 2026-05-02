5a8e981eaf0be7a4bb7c1986c9a3d857a4afcf7f
xiong <xiongyx48@gmail.com>
2026-05-02 21:09:55 +0800

    refactor: 删除多余的开发者工具配置

--- KugouAvaloniaPlayer/Program.cs
+++ KugouAvaloniaPlayer/Program.cs
@@ -32,9 +32,6 @@ internal sealed class Program
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-#if DEBUG
-            .WithDeveloperTools()
-#endif
             .WithInterFont()
             .LogToTrace();
 }
